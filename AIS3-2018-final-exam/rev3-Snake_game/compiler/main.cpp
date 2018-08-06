d0e849452964910bec6bfc92ca23775f3baf0238
WildfootW <wild.foot.yee.tzwu@gmail.com>
2018-08-06 15:52:29 +0800

    add AIS3-2018-final

---
+++ AIS3-2018-final-exam/rev3-Snake_game/compiler/main.cpp
@@ -0,0 +1,11 @@
+#include"compiler.h"
+#include"builtin_class.h"
+#include"snake_class.h"
+Begin
+    #include"builtin_lib.h"
+    #include"snake_lib.h"
+    Output::Init();
+    Math::Init();
+    Array::Init();
+    Snake::Main();
+Finish
---
+++ AIS3-2018-final-exam/rev3-Snake_game/compiler/snake_class.h
@@ -0,0 +1,10 @@
+class Snake{
+    public:
+    static void Main(){
+        Var p;
+        p=Array::Alloc(30);
+        String::Set(p,"8787UR87");
+        Output::PrintString(p);
+        while_true();
+        }
+    };
